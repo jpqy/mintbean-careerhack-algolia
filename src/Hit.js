c221991005809c47437667b7324c1966b448eeaa
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-06-03 15:39:15 -0400

    change pokedex numbers to badges

--- src/Hit.js
+++ src/Hit.js
@@ -8,16 +8,14 @@ function Hit({ hit }) {
   return (
     <div className="hit">
       <div className="hit-image">
+        <aside className="pokedex-number">{hit.id}</aside>
         <img src={hit.img} />
       </div>
       <div className="hit-content">
         <div>
           <div className="hit-name">
             <Highlight attribute="name" hit={hit} tagName="em" />
-          </div>
-          <span className="hit-num">
-            <Highlight attribute="num" hit={hit} />
-          </span>
+          </div>          
         </div>
         <div className="hit-type">{hit.type.join(", ")}</div>
       </div>
