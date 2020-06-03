42042289c703e4c3556dafe23d1e398cb6aaccbd
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-06-03 15:46:47 -0400

    add pokeball icon in search stats

--- src/Stats.js
+++ src/Stats.js
@@ -4,10 +4,13 @@ export default connectStateResults(({ searchResults }) => {
   if (searchResults === null || searchResults.nbHits === 0) return <div />;
   return (
     <div className="ais-Stats">
-      <span role="img" aria-label="emoji">
-        ⚡️
-      </span>{" "}
-      <strong>{searchResults.nbHits}</strong> results found{" "}
+      <img
+        src="img/pokeball.png"
+        role="img"
+        aria-label="pokeball-icon"
+        id="pokeball-icon"
+      />{" "}
+      <strong>{searchResults.nbHits}</strong> Pokemon found{" "}
       {searchResults.query !== "" && (
         <React.Fragment>
           for <strong>{`"${searchResults.query}"`}</strong>
