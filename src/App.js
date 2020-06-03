221e4f60f1119e841b27576304ba8987c596b214
Jimmy Peng <29219382+jpqy@users.noreply.github.com>
2020-06-03 14:55:54 -0400

    add side-panels

--- src/App.js
+++ src/App.js
@@ -7,24 +7,18 @@ import {
   Panel,
 } from "react-instantsearch-dom";
 import "./App.css";
-import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from "algoliasearch/lite";
 import Stats from "./Stats.js";
 import RangeSlider from "./RangeSlider.js";
 import Content from "./Content";
 import Facet from "./Facet";
 const APPLICATION_ID = "X2H6EHSA90";
 const SEARCH_API = "ad06c17de6f6949cfc0f8ef3a7ba1616";
-const searchClient = algoliasearch(
-  APPLICATION_ID,
-  SEARCH_API
-);
+const searchClient = algoliasearch(APPLICATION_ID, SEARCH_API);
 
 function App() {
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName="pokemon"
-    >
+    <InstantSearch searchClient={searchClient} indexName="pokemon">
       <main className="search-container">
         <Configure
           hitsPerPage={10}
@@ -38,9 +32,7 @@ function App() {
           </div>
           <div id="searchbox">
             {/* Uncomment the following widget to add a search bar */}
-            {/* <SearchBox
-                translations={{ placeholder: "Search for products" }}
-              /> */}
+            <SearchBox translations={{ placeholder: "Search for products" }} />
           </div>
           <div id="stats">
             {/* Uncomment the following widget to add search stats */}
@@ -52,28 +44,18 @@ function App() {
           </div>
         </div>
         <div className="left-panel">
-          <div id="categories">
+          <div id="type">
             {/* Uncomment the following widget to add categories list */}
-            {/* <Panel header="Categories">
-                <Facet attribute="categories" />
-              </Panel> */}
+            <Panel header="Type">
+              <Facet attribute="type" searchable={true} />
+            </Panel>
           </div>
-          <div id="brands">
+          <div id="weaknesses">
             {/* Uncomment the following widget to add brands list */}
-            {/* <Panel header="Brands">
-                <Facet
-                  attribute="brand"
-                  searchable={true}
-                  translations={{ placeholder: "Search for other..." }}
-                />
-              </Panel> */}
-          </div>
-          <div id="price">
-            {/* Uncomment the following widget to add price */}
-            {/* <Panel header="Price">
-                <RangeSlider attribute="price" />
-              </Panel> */}
-          </div>
+            <Panel header="Weaknesses">
+              <Facet attribute="weaknesses" searchable={true} />
+            </Panel>
+          </div>          
         </div>
       </main>
     </InstantSearch>
